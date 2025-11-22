5ea30da4d73648f3c64a86e916672bd10e316bae
zhuochenghe <jonzhe@umich.edu>
2025-11-22 18:33:55 +0000

    Set lora_A and lora_B biasless. Fix merge and unmerge of dora

--- dora_implementation/dora.py
+++ dora_implementation/dora.py
@@ -1,16 +1,16 @@
 import torch.nn as nn
 import torch
-from lora_implementation import LoRA_Linear
 from torch.nn import functional as F
+from lora_implementation import LoRA_Linear
     
 class Dora_Linear(LoRA_Linear):
     def __init__(
             self, 
+            base_linear,
             r=4, 
             lora_alpha=1.0,
             bias=True,
             lora_dropout=0.0,
-            base_linear: nn.Linear = None,
             merged : bool = False,
             **kwargs,
             ):
@@ -22,10 +22,6 @@ class Dora_Linear(LoRA_Linear):
             lora_dropout=lora_dropout,
         )
         self.weight_m_wdecomp = nn.Linear(1,self.out_features,bias=False)
-        if base_linear is not None:
-            self.linear.weight = base_linear.weight
-            if bias and base_linear.bias is not None:
-                self.linear.bias = base_linear.bias
 
     def forward(self, x):
         previous_dtype = x.dtype
@@ -38,29 +34,33 @@ class Dora_Linear(LoRA_Linear):
             if not self.linear.bias is None:
                 result += self.linear.bias.view(1, -1).expand_as(result)
 
-            result += ( norm_scale * (self.lora_B(self.lora_A(dropout_x.to(self.lora_A.weight.dtype))))) * self.scaling
+            result += ( norm_scale * (self.lora_B(self.lora_A(dropout_x)))) * self.scaling
         else:
              result = F.linear(x, self.linear.weight, bias=self.linear.bias)
         
-        if result.dtype != previous_dtype:
-            result = result.to(previous_dtype)
-
         return result
-    
+        
+    @torch.no_grad
     def merge(self):
         if self.r > 0 and not self.merged:
+            self._orig_weight = self.linear.weight.data.clone().detach()
             new_weight_v = self.linear.weight + (self.lora_B.weight @ self.lora_A.weight) * self.scaling
             norm_scale = self.weight_m_wdecomp.weight.view(-1) / (torch.linalg.norm(new_weight_v,dim=1)).detach()
-            self.linear.weight.data = self.linear.weight.data * norm_scale.view(-1,1)
-            self.linear.weight.data += (self.lora_B.weight @ self.lora_A.weight) * self.scaling * norm_scale.view(-1,1)
+            self.linear.weight.data = new_weight_v * norm_scale.view(-1,1)
+            # self.linear.weight.data += (self.lora_B.weight @ self.lora_A.weight) * self.scaling * norm_scale.view(-1,1)
             self.merged = True
 
+    @torch.no_grad
     def unmerge(self):
         if self.r > 0 and self.merged:
-            new_weight_v = self.linear.weight - (self.lora_B.weight @ self.lora_A.weight) * self.scaling
-            norm_scale = self.weight_m_wdecomp.weight.view(-1) / (torch.linalg.norm(new_weight_v,dim=1)).detach()
-            self.linear.weight.data = (self.linear.weight.data - (self.lora_B.weight @ self.lora_A.weight) * self.scaling * norm_scale.view(-1,1)) / norm_scale.view(-1,1)
-            self.merged = False
+            if hasattr(self, "_orig_weight"):
+                self.linear.weight.data = self._orig_weight
+                del self._orig_weight
+                self.merged = False
+            # new_weight_v = self.linear.weight + (self.lora_B.weight @ self.lora_A.weight) * self.scaling
+            # norm_scale = self.weight_m_wdecomp.weight.view(-1) / (torch.linalg.norm(self.linear.weight,dim=1)).detach()
+            # self.linear.weight.data = self.linear.weight.data / norm_scale.view(-1, 1) - (self.lora_B.weight @ self.lora_A.weight) * self.scaling
+            # self.linear.weight.data = (self.linear.weight.data - (self.lora_B.weight @ self.lora_A.weight) * self.scaling * norm_scale.view(-1,1)) / norm_scale.view(-1,1)
 
 
     
--- lora_implementation/lora.py
+++ lora_implementation/lora.py
@@ -3,7 +3,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 class LoRA_Linear(nn.Module):
-    def __init__(self, base_linear, r=0, lora_alpha=1, lora_dropout=0.0, **kwargs):
+    def __init__(self, base_linear, r=0, lora_alpha=1.0, lora_dropout=0.0, **kwargs):
         super().__init__()
 
         self.in_features = base_linear.in_features
@@ -20,8 +20,8 @@ class LoRA_Linear(nn.Module):
         self.merged = False
 
         if r > 0:
-            self.lora_A = nn.Linear(self.in_features, r)
-            self.lora_B = nn.Linear(r, self.out_features)
+            self.lora_A = nn.Linear(self.in_features, r, bias=False)
+            self.lora_B = nn.Linear(r, self.out_features, bias=False)
             nn.init.normal_(self.lora_A.weight)
             nn.init.zeros_(self.lora_B.weight)
 
@@ -37,11 +37,13 @@ class LoRA_Linear(nn.Module):
         else:
             return self.linear(x)
 
+    @torch.no_grad
     def merge(self):
         if self.r > 0 and not self.merged:
             self.linear.weight.data += (self.lora_B.weight @ self.lora_A.weight) * self.scaling
             self.merged = True
 
+    @torch.no_grad
     def unmerge(self):
         if self.r > 0 and self.merged:
             self.linear.weight.data -= (self.lora_B.weight @ self.lora_A.weight) * self.scaling
