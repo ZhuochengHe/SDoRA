5736e9d7d98ecb25ca1b589e9e47287fb1fa9659
clz123456clz <clz123456clz@sjtu.edu.cn>
2025-11-23 02:33:20 +0000

    Update sdora testing

--- __init__.py
+++ __init__.py
@@ -1,3 +1,4 @@
 from .lora_implementation import LoRA_Linear
 from .sora_implementation import SoRA_Linear
 from .dora_implementation import DoRA_Linear
+from .sd_lora_implementation import SDoRA_Linear
